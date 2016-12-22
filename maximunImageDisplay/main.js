2e8633552e0c2b10b8995eca6e6e21e7ba489968
Joseph Su <joseph.su@mitake.com.tw>
2016-12-22 13:12:36 +0800

    imgload

--- maximunImageDisplay/main.js
+++ maximunImageDisplay/main.js
@@ -1,33 +1,36 @@
 $(function(){
 
+	var memContainer = $(".contact-mems");
+	var arr = [];
 	for(var i=0;i<1000;i++){
-		var img_url = "http://fakeimg.pl/70x70/000?"+Math.floor((Math.random()*1000)+1);
+		if(i < 80){
+			var img_url = "http://fakeimg.pl/70x70/ccc?"+Math.floor((Math.random()*100000)+1);
+		}else if(i >= 80 && i<160){
+			var img_url = "http://fakeimg.pl/70x70/FF5733?"+Math.floor((Math.random()*100000)+1);
+		}else{
+			var img_url = "http://fakeimg.pl/70x70/000?"+Math.floor((Math.random()*100000)+1);
+		}
+		
 		var grid = "<div class=\"mem\">"+
-		            "<div class=\"img waitLoad\" data-url=\""+img_url+"\"></div>"+
+		            "<div class=\"img\" style=\"background-image:url("+img_url+")\"></div>"+
 		        	"<div class=\"name\">test</div>"+
 		        "</div>";
-		$(".contact-mems").append(grid);
+		arr.push(grid);
+		// $(".contact-mems").append(grid);
 	}
-  	
-  	var tmp = $(".contact-mems").find(".img.waitLoad:lt(100)");
-	$.each(tmp, function(index,domTmp){
-		var dom = $(domTmp);
-		dom.css("background-image","url("+dom.attr("data-url")+")").removeClass("waitLoad").removeAttr("data-url");
-	});
 
-	var memContainer = $(".contact-mems");
-	var g_contactWaitLoadImgs = memContainer.find(".img.waitLoad");
+
+  	console.log(arr);
+
 	memContainer.scroll(function(e) {
-		if( null == g_contactWaitLoadImgs) return;
-		
-		var height = $(this).height()+100;
-		for(var i=g_contactWaitLoadImgs.length-1;i>=0;i--){
-			var tmpDom = $(g_contactWaitLoadImgs[i]);
-			if(tmpDom.offset().top < height){
-				tmpDom.css("background-image","url("+tmpDom.attr("data-url")+")").removeClass("waitLoad").removeAttr("data-url");
-				g_contactWaitLoadImgs.splice(i,1);
-			}
-		}
+		var height = $(this).height();	//500
+		var scrollTop = $(this).scrollTop();	
+  		console.log(height,scrollTop);
+  		// console.log(Math.floor(scrollTop / height));
+  		if(scrollTop > height){
+  			var test = Math.floor(scrollTop / height);
+  			// $(".contact-mems").append();
+  		}
 	});
 })
 
